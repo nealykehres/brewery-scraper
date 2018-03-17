0213d1d2bd8b72a3c890d0e3da09c05db74755be
Nealy Kehres <31978373+nealykehres@users.noreply.github.com>
2018-03-16 23:24:30 -0400

    second draft

--- breweries2.py
+++ breweries2.py
@@ -1,4 +1,5 @@
 import time
+import csv
 from selenium import webdriver
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
@@ -6,8 +7,11 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+import datetime
+import shutil
 
 driver = webdriver.Chrome('/Users/Nealy/Documents/web-apps-2/scraping/chromedriver')
+
 driver.get('https://www.brewersassociation.org/directories/breweries/');
 
 # page_source is a variable created by Selenium - it holds all the HTML
@@ -31,14 +35,67 @@ WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID,"status-ba
 #added this sleep because I thought that BeautifulSoup might be scraping quicker than the information loaded
 time.sleep(5)
 
-bsObj = BeautifulSoup(html, "html.parser")
+# bsObj = BeautifulSoup(html, "html.parser")
+innerHTML = driver.execute_script("return document.body.innerHTML")
+bsObj = BeautifulSoup(innerHTML, "html.parser")
 
 
+brewery_boxes = bsObj.findAll("div", {"class":"brewery"})
 
-#Prof McAdams: Sorry for the weird names.
-list2 = bsObj.find("div", {"class":"entry"})
-cookie = list2.find("div", {"id":"ajax-content"})
+#this function goes through all of the brewery boxes and finds the information for each brewery then adds it to a list.
 
-print(cookie)
+url_list = []
 
-#driver.quit()
+def get_info(brewery):
+
+    name = brewery.find("li", {"class":"name"}).get_text()
+    address = brewery.find("li", {"class":"address"}).get_text()
+    city = brewery.find("li", {'class': None}).get_text()
+    brewery_type = brewery.find("li", {"class":"brewery_type"}).get_text()
+    try:
+        url = brewery.find("li", {"class":"url"}).find("a")["href"]
+    except AttributeError:
+        url = brewery.find("li", {"class":"url"})
+    #url_boxes = bsObj.findAll("li", {"class":"url"}).find("a")["href"]
+    #for url in url_boxes:
+        #url_list.append(url.find("a")["href"])
+
+    brewery_list = [name, address, city, brewery_type, url]
+    return brewery_list
+
+#needed to write a new function in order to append the URLS to a list
+def get_list(links):
+    try:
+        url = brewery.find("li", {"class":"url"}).find("a")["href"]
+    except AttributeError:
+        url = brewery.find("li", {"class":"url"})
+    url_list.append(url)
+    #print(url_list)
+
+#now write the info to the csv file and append to the url list
+breweries_file = open('breweries_file.csv', 'w')
+writer = csv.writer(breweries_file)
+writer.writerow(("brewery", "address", "city", "type", "url"))
+for brewery in brewery_boxes[:100]:
+    writer.writerow(get_info(brewery))
+    get_list(brewery)
+    #url_list.append(brewery.findAll("li", {"class":"url"}))
+
+breweries_file.close()
+
+#now get screenshots
+for url in url_list:
+    date_stamp = str(datetime.datetime.now()).split('.')[0]
+    date_stamp = date_stamp.replace(" ", "_").replace(":", "_").replace("-", "_")
+    file_name= date_stamp + ".png"
+    try:
+        driver.get(url)
+        driver.get_screenshot_as_file(file_name)
+        #move file to external hardrive
+        shutil.move(file_name, "/Volumes/My Passport for Mac/brewerypics")
+    #sleep 1 second just to make sure each file name is unique.
+        time.sleep(1)
+    except:
+        print("None available")
+
+driver.close()
